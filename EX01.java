a81c7158a9606644f42b2537e9ab5a319ed1300e
SecretGmG <86920571+SecretGmG@users.noreply.github.com>
2022-10-06 16:57:18 +0200

    Add files via upload

---
+++ EX01.java
@@ -0,0 +1,11 @@
+//Sigrist Cedric 22-120-844
+//Moritz Scholz 22-122-071
+
+public class EX01 {
+
+	public static void main(String[] args) {
+		JonSnow.RunBothVersions();
+		UselessFormula.GetInputsPrintCalculations();
+		TemperatureJudge.PromptAndJudge();
+	}
+}
---
+++ JonSnow.java
@@ -0,0 +1,20 @@
+//Sigrist Cedric 22-120-844
+//Moritz Scholz 22-122-071
+
+public class JonSnow {
+	public static void Version1() {
+		System.out.println("Winter is coming");
+	}
+	public static void Version2() {
+		System.out.println("Winter\nis\ncoming");
+	}
+	
+	public static void RunBothVersions() {
+		System.out.println("Version 1:");
+		Version1();
+		System.out.println("Version 2:");
+		Version2();
+		
+	}
+	
+}
